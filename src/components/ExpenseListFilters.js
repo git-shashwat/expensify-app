3e4171d44b63ba20507dc13223412e618e3f900a
git-shashwat <shashwat824@gmail.com>
2020-01-15 17:10:51 +0530

    Add Expensify Styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -18,33 +18,49 @@ class ExpenseListFilters extends React.Component {
     };
     render() {
         return (
-            <div>
-        <input type="text" value = {this.props.filters.text} onChange={(e) => {
-            this.props.dispatch(setTextFilter(e.target.value));
-        }}/>
-        <select value={this.props.filters.sortBy} onChange={(e) => {
-            if (e.target.value === "amount"){
-                this.props.dispatch(sortByAmount());
-            }
-            else if (e.target.value === "date") {
-                this.props.dispatch(sortByDate());
-            }
-        }}>
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
-            startDate={this.props.filters.startDate}
-            endDate={this.props.filters.endDate}
-            onDatesChange={this.onDatesChange}
-            focusedInput={this.state.calendarFocused}
-            onFocusChange={this.onFocusChange}
-            showClearDates={true}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-            startDateId="start_cool"
-            endDateId="end_cool"
-        />
+            <div className="content-container">
+            <div className="input-group">
+                <div className="input-group__item">
+                    <input
+                     className="text-input"
+                     placeholder="Search expenses" 
+                     type="text" 
+                     value = {this.props.filters.text} 
+                     onChange={(e) => {
+                        this.props.dispatch(setTextFilter(e.target.value));
+                    }}/>
+                </div>
+                <div className="input-group__item">
+                    <select
+                     className="select"
+                     value={this.props.filters.sortBy}
+                     onChange={(e) => {
+                        if (e.target.value === "amount"){
+                            this.props.dispatch(sortByAmount());
+                        }
+                        else if (e.target.value === "date") {
+                            this.props.dispatch(sortByDate());
+                        }
+                    }}>
+                        <option value="date">Date</option>
+                        <option value="amount">Amount</option>
+                    </select>
+                </div>
+                <div className="input-group__item">
+                    <DateRangePicker
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                    startDateId="start_cool"
+                    endDateId="end_cool"
+                />
+                </div>
+            </div>
     </div>
         )
     }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { startLogin } from '../actions/auth';
 
 const LoginPage =  ({ startLogin }) => {
     return (
-        <div>
-            <button type="submit" onClick={startLogin}>Login</button>
+        <div className="box-layout">
+            <div className="box-layout__box">
+                <h1 className="box-layout__title">Expensify</h1>
+                <p>It's time to get your expenses under control.</p>
+                <button className="button" type="submit" onClick={startLogin}>Login with Google</button>
+            </div>
         </div>
     );
 };
